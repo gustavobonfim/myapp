13ab8dd7aafcbfc0169a2d962c5f399e10eaa701
gustavobonfim <bonfimoliveira@gmail.com>
2020-10-15 21:59:23 -0300

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_001133) do
+ActiveRecord::Schema.define(version: 2020_10_16_005854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2020_10_16_001133) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.json "payload"
+  end
+
+  create_table "operation_product_bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
